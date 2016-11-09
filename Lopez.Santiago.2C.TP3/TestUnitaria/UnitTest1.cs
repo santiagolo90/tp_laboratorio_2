2f42cbe5218b608f7196b3508d81fe33d20a18a0
santiagolo90 <santiago.lo90@gmail.com>
2016-11-09 16:47:34 -0300

    Lopez.Santiago2C-TP3 Laboratorio2 (faltan test unitaria)

--- Lopez.Santiago.2C.TP3/TestUnitaria/UnitTest1.cs
+++ Lopez.Santiago.2C.TP3/TestUnitaria/UnitTest1.cs
@@ -1,5 +1,13 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntidadesAbstractas;
+using EntidadesInstanciables;
+using Archivos;
+using Excepciones;
 
 namespace TestUnitaria
 {
@@ -7,8 +15,32 @@ namespace TestUnitaria
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void AlumnoTest()
         {
+            Alumno a1 = new Alumno(1, "Juan", "Lopez", "12234456",
+            EntidadesAbstractas.Persona.ENacionalidad.Argentino, Gimnasio.EClases.CrossFit, EntidadesInstanciables.Alumno.EEstadoCuenta.MesPrueba);
+
+            Assert.IsNotNull(a1.Nombre);
+            Assert.IsNotNull(a1.Apellido);
+            Assert.IsNotNull(a1.DNI);
+            Assert.IsNotNull(a1.Nacionalidad);
+
+            
+
+
+        }
+
+        [TestMethod]
+        public void InstructorTest()
+        {
+            Instructor i1 = new Instructor(1, "Juan", "Lopez", "12234456",
+            EntidadesAbstractas.Persona.ENacionalidad.Argentino);
+
+            Assert.IsNotNull(i1.Nombre);
+            Assert.IsNotNull(i1.Nombre);
+            Assert.IsNotNull(i1.DNI);
+            Assert.IsNotNull(i1.Nacionalidad);
+
         }
     }
 }
